adfe9c778874af6eddec1f3d94d9d443375d25b1
Vinish <mm7920460@gmail.com>
2026-02-27 19:55:03 +0530

    Add file analysis functionality to extract modules from selected PDFs

--- HelpMePass-v2-js/js/app.js
+++ HelpMePass-v2-js/js/app.js
@@ -1,8 +1,41 @@
 import * as pdfjsLib from "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/5.4.149/pdf.min.mjs";
-
+import { extractModulesFromText } from "./parser";
+import { renderResults } from "./ui";
 
 pdfjsLib.GlobalWorkerOptions.workerSrc =  "https://cdnjs.cloudflare.com/ajax/libs/pdf.js/5.4.149/pdf.worker.min.mjs";
 
+
+const analyzeBtn = document.getElementById("analyzeBtn");
+analyzeBtn.addEventListener("click", analyzeFiles);
+
+async function analyzeFiles() {
+    const input = document.getElementById("files");
+    const files = input.files;
+
+    if(!files.length){
+        alert("Please Select PDF files first.");
+        return;
+    }
+
+    let allModules = {};
+
+    for (let file of files){
+        const text = await extractModulesFromPDF(file);
+        const modules = extractModulesFromText(text);
+
+        for(let moduleName in modules) {
+            if (!allModules[moduleName]) {
+                allModules[moduleName] = [];
+            }
+
+            allModules[moduleName].push({
+                paper: file.name,
+                content: modules[moduleName]
+            });
+        }
+    }
+    renderResults(allModules);
+}
 
 export async function extractTextFromPDF(file) {
     const arryBuffer = await file.arrayBuffer();
