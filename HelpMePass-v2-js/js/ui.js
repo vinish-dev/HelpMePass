b903de9d9766bf2bf89687818416d5072f82efb1
Vinish <mm7920460@gmail.com>
2026-03-18 00:55:53 +0530

    Refactor renderResults function in ui.js to improve module handling and enhance keyword highlighting

--- HelpMePass-v2-js/js/ui.js
+++ HelpMePass-v2-js/js/ui.js
@@ -1,25 +1,61 @@
+let matchCounter = 0;
+
 export function renderResults(data) {
+    matchCounter = 0;
     const resultdiv = document.getElementById("results");
     resultdiv.innerHTML = "";
 
-    for (module in data) {
+    for (let moduleName in data) {
         const moduleDiv = document.createElement("div");
         moduleDiv.className = "module";
 
         const title = document.createElement("h2");
-        title.textContent  = module;
+        title.textContent = moduleName;
         moduleDiv.appendChild(title);
 
-        data[module].forEach(entry => {
+
+        const details = document.createElement("details");
+        details.open = true;
+
+        const summary = document.createElement("summary");
+        summary.textContent = moduleName;
+
+        details.appendChild(summary);
+        moduleDiv.appendChild(details);
+
+
+        data[moduleName].forEach(entry => {
             const paperTitle = document.createElement("h4")
             paperTitle.textContent = `From ${entry.paper}`;
-            moduleDiv.appendChild(paperTitle);
+            details.appendChild(paperTitle);
 
             const pre = document.createElement("pre");
-            pre.textContent = entry.content;
-            moduleDiv.appendChild("pre");
+            // pre.textContent = entry.content; //old 
+            const keyword = document.getElementById("searchBox").value;
+            pre.innerHTML = formatContent(heighlightKeyword(entry.content, keyword));
+            details.appendChild(pre);
         });
-        
+
         resultdiv.appendChild(moduleDiv);
     }
+    document.getElementById("matchCount").textContent = `🔎 Matches found: ${matchCounter}`;
+}
+
+function heighlightKeyword(text, keyword) {
+
+    if (!keyword || keyword.trim() == "") return text; //don't highlight or count
+
+    const escapedKeyword = keyword.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
+    const regex = new RegExp(`(${escapedKeyword})`, "gi");
+
+    return text.replace(regex, (match) => {
+        matchCounter++;
+        return `<span class="highlight">${match}</span>`;
+    });
+}
+
+function formatContent(text) {
+    return text
+        .replace(/OR/g, "<span class='or'>OR</span>")
+        .replace(/\n/g, "<br>");
 }
