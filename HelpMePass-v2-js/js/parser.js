9c6139c26bac704272563b06a38e70546deb6b75
Vinish <mm7920460@gmail.com>
2026-03-20 19:35:07 +0530

    Refactor cleanText function and improve regex logic

--- HelpMePass-v2-js/js/parser.js
+++ HelpMePass-v2-js/js/parser.js
@@ -29,9 +29,13 @@ function cleanText(text) {
     return text
         .replace(/\s{2,}/g, " ")
         .replace(/\b(OR)\b/g,"\n$1\n")
-        .replace(/(\d+\s+[ab]\))/g, "\n$1")
+        .replace(/(?:[\n\s])*\b(\d{1,2})\s+(\(?\b(?:[b-f]|ii{1,2}|iv|v|vi)\s*\))/ig, " $1\n$2")
+        .replace(/(?:[,\t ])*(\(?\b(?:\d+[ \t]+[a-z]|[a-z]|i{1,3}|iv|v|vi)\s*\))/ig, "\n$1")
         .replace(/\n\s+/g, "\n")
-        .replace(/\d+\s*\|\s*\d+\s*Page?/gi, "")
+        .replace(/ST JOSEPH ENGINEERING COLLEGE.*?P\s*a\s*g\s*e[\sA-Z0-9]*/gi, "")
+        .replace(/\d+\s*\|\s*\d+\s*P\s*a\s*g\s*e(?:\s+[A-Z0-9]+)?/gi, "")
+        .replace(/\b\d?\s*\d\s*[A-Z]\s*[A-Z]\s*[A-Z]\s*(?:[A-Z]\s*)?\d\s*\d\s*\d\b/gi, "")
+        .replace(/\b\d{2}[A-Z]{2,4}\d{2,4}\b/gi, "")
         .replace(/\b\d+\s*BFE\s*\d+/gi, "")
         .replace(/Q\.No.*Marks/gi, "")
         .trim();
